22b10276a354f3f59f5bdd47d73b384723180712
todorilievski <todor.ilievski@byteworx.de>
2022-10-04 20:49:44 +0200

    initial commit basic app

---
+++ src/test/java/allange/contract/leasing/LeasingContractApplicationTests.java
@@ -0,0 +1,13 @@
+package allange.contract.leasing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeasingContractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
